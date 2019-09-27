a37892800bd8c983571682b7741ae7a156e4941b
Alan <alan@dymaxionlabs.com>
2019-09-27 14:39:25 -0300

    raise exceptions if env vars are not defined

--- src/terra/__init__.py
+++ src/terra/__init__.py
@@ -17,8 +17,14 @@ except DistributionNotFound:
 finally:
     del get_distribution, DistributionNotFound
 
-API_KEY = os.getenv('TERRA_API_KEY')
-
 API_URL = os.getenv('TERRA_API_URL', 'https://api.dymaxionlabs.com')
 
+API_KEY = os.getenv('TERRA_API_KEY')
+if API_KEY is None:
+    raise Exception('TERRA_API_KEY not defined')
+
 PROJECT_ID = os.getenv('TERRA_PROJECT_ID')
+if PROJECT_ID is None:
+    raise Exception('TERRA_PROJECT_ID not defined')
+
+
