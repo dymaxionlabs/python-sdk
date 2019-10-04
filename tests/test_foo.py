542813db07b2c0751cb68258dfb7f780992432e0
Damián Silvani <munshkr@gmail.com>
2019-10-04 09:13:43 -0300

    Add a simple test for setting up TravisCI

---
+++ tests/test_foo.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+from dymaxionlabs import model
+from dymaxionlabs import file
+
+__author__ = "Dymaxion Labs"
+__copyright__ = "Dymaxion Labs"
+__license__ = "apache-2.0"
+
+
+def test_true():
+    assert True
