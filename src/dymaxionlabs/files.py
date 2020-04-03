6a074a726756c39e1386b0d16a37a9eeb3a8b70f
Damián Silvani <munshkr@gmail.com>
2020-04-03 18:15:20 -0300

    Refactor File class

--- src/dymaxionlabs/files.py
+++ src/dymaxionlabs/files.py
@@ -1,91 +1,80 @@
-import json
 import mimetypes
-import requests
 import os
-import urllib3
-from dymaxionlabs.utils import get_api_url, get_api_key
 
-DYM_UPLOAD_FILE = '/files/upload/{file_name}?project_uuid={project_uuid}'
-DYM_DOWNLOAD_FILE = '/files/download/{file_name}?project_uuid={project_uuid}'
+from dymaxionlabs.utils import fetch_from_list_request, request
 
 
 class File:
-    def __init__(self, project, name, metadata):
-        """Constructor
+    base_path = '/files'
+
+    def __init__(self, name, metadata, **extra_attributes):
+        """The File class represents files stored in Dymaxion Labs.
+
+        Files are owned by the authenticated user.
 
         Args:
-            project: related project
-            name: image's name in DymaxionLabs's server
-            metada: image metada
+            name: file name
+            metadata: file metadata
+            extra_attributes: extra attributes from API endpoint
+
         """
-        self.project = project
         self.name = name
         self.metadata = metadata
+        self.extra_attributes = extra_attributes
 
-    def download(output_dir):
+    @classmethod
+    def all(cls):
+        return [File(**attrs) for attrs in fetch_from_list_request(cls.base_path + '/')]
+
+    @classmethod
+    def get(cls, name):
+        attrs = request(
+            'get', '{base_path}/{name}'.format(base_path=cls.base_path, name=name))
+        return File(**attrs)
+
+    def delete(self):
+        request(
+            'delete', '{base_path}/{name}'.format(base_path=self.base_path,
+                                                  name=self.name))
+        return True
+
+    @classmethod
+    def upload(cls, input_path):
+        """Upload a file to storage
+
+        Args:
+            filename -- path to local file
+
+        Raises:
+            FileNotFoundError: Path
+        """
+        filename = os.path.basename(input_path)
+        with open(input_path, 'rb') as fp:
+            data = fp.read()
+        path = '{base_path}/upload/{filename}'.format(
+            base_path=cls.base_path, filename=filename)
+        response = request('post', path, headers={
+            'Content-Type': mimetypes.MimeTypes().guess_type(filename)[0]
+        }, body=data, binary=True)
+        return response
+
+    def download(self, output_dir="."):
         """Download file and save it to +output_dir+
 
-        If the directory does not exist it will be created.
+        If +output_dir+ does not exist, it will be created.
 
         Args:
             output_dir: path to store file
         """
-        download(self.name, output_dir)
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        path = '{base_path}/download/{name}'.format(
+            base_path=self.base_path,
+            name=self.name)
+        content = request('get', path, binary=True, parse_response=False)
+        output_file = os.path.join(output_dir, self.name)
+        with open(output_file, 'wb') as f:
+            f.write(content)
 
     def __repr__(self):
-        return "<dymaxionlabs.file.File name=\"{name}\"".format(name=self.name)
-
-
-def upload(filename):
-    """Upload a file named +filename+
-
-    Args:
-        filename -- path to local file
-
-    Returns:
-        Returns the detail of the object that was created in DymaxionLabs's server
-
-    Raises:
-        FileExistsError: The filename argument does not correspond to an existing file
-    """
-    if os.path.isfile(filename) and os.access(filename, os.R_OK):
-        http = urllib3.PoolManager()
-        headers = {
-            'Authorization': 'Api-Key {}'.format(get_api_key()),
-            'Accept-Language': 'es',
-            'Content-Type': mimetypes.MimeTypes().guess_type(filename)[0]
-        }
-        upload_url = DYM_UPLOAD_FILE.format(
-            file_name=os.path.basename(filename),
-            project_uuid=get_project_id())
-        url = '{url}{path}'.format(url=get_api_url(), path=upload_url)
-        with open(filename, 'rb') as fp:
-            file_data = fp.read()
-        r = http.request('POST', url, body=file_data, headers=headers)
-        return json.loads(r.data.decode('utf-8'))['detail']
-    else:
-        raise FileExistsError
-
-
-def download(filename, output_dir="."):
-    """Download a file named +filename+ to +output_dir+
-
-    If the output directory does not exist it will be created.
-
-    Args:
-        filename: image name
-        output_dir: local destination to store the image
-    """
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-    headers = {
-        'Authorization': 'Api-Key {}'.format(get_api_key()),
-        'Accept-Language': 'es'
-    }
-    download_url = DYM_DOWNLOAD_FILE.format(
-        file_name=os.path.basename(filename), project_uuid=get_project_id())
-    url = '{url}{path}'.format(url=get_api_url(), path=download_url)
-    r = requests.get(url, headers=headers)
-    output_file = os.path.sep.join([output_dir, filename])
-    with open(output_file, 'wb') as f:
-        f.write(r.content)
+        return "<File name={name!r}".format(name=self.name)
