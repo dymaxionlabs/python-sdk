ba57ba4da54bcc662222c10397aa7a0c357694c1
Gessica <gessica@dymaxionlabs.com>
2020-07-02 16:33:32 -0300

    test File.all

--- tests/test_file.py
+++ tests/test_file.py
@@ -1,12 +1,6 @@
-from unittest.mock import patch
-import pytest
-import dymaxionlabs
 import unittest
-import responses
-import requests
-
+from unittest.mock import patch
 from dymaxionlabs.files import File
-from dymaxionlabs.utils import NotFoundError
 
 __author__ = "Dymaxion Labs"
 __copyright__ = "Dymaxion Labs"
@@ -14,24 +8,18 @@ __license__ = "apache-2.0"
 
 class FileTest(unittest.TestCase):
     def setUp(self):
-        print("setUp")
+        self.test_file = File('test', '/test', 'metadata')
 
     def tearDown(self):
-        print("tearDown")
-    
-    @responses.activate
-    @patch("dymaxionlabs.utils.request")
+        del self.test_file
+
+    @patch("dymaxionlabs.files.request")
     def test_all(self, mock_request):
-        test_file = File('foo.txt', '/', 'metadata')
-        path="*"
-        response = {'error': 'not found'}
-        responses.add(responses.GET, 'https://api.dymaxionlabs.com{base_path}/files/?path={path}'.format(
-                base_path=test_file.base_path, path=requests.utils.quote(path)),
-                  json=response, status=404)
-        self.assertIs(mock_request, dymaxionlabs.utils.request)
-        try:
-            test_file.all()
-        except Exception as ex:
-            self.assertEqual(type(ex), NotFoundError)
-        self.assertEqual(len(responses.calls), 1)
-        self.assertEqual(responses.calls[0].response.text, '{"error": "not found"}')
+        mock_request.return_value = [
+            {'name':'foo', 'path':'/foo', 'metadata':'metadata_foo'},
+            {'name':'bar', 'path':'/bar', 'metadata':'metadata_bar'},
+        ]
+        rv = self.test_file.all()
+        names = [f.name for f in rv]
+        mock_request.assert_called_once()
+        self.assertListEqual(names, ['foo', 'bar'])
