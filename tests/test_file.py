e0b8d94758bba49aa17ac531bf5280ab4630e86a
Gessica <gessica@dymaxionlabs.com>
2020-07-06 12:26:51 -0300

    add file test all, test get and test delete

--- tests/test_file.py
+++ tests/test_file.py
@@ -1,4 +1,5 @@
 import unittest
+from requests.utils import quote
 from unittest.mock import patch
 from dymaxionlabs.files import File
 
@@ -7,11 +8,6 @@ __copyright__ = "Dymaxion Labs"
 __license__ = "apache-2.0"
 
 class FileTest(unittest.TestCase):
-    def setUp(self):
-        self.test_file = File('test', '/test', 'metadata')
-
-    def tearDown(self):
-        del self.test_file
 
     @patch("dymaxionlabs.files.request")
     def test_all(self, mock_request):
@@ -19,7 +15,37 @@ class FileTest(unittest.TestCase):
             {'name':'foo', 'path':'/foo', 'metadata':'metadata_foo'},
             {'name':'bar', 'path':'/bar', 'metadata':'metadata_bar'},
         ]
-        rv = self.test_file.all()
+        rv = File.all()
         names = [f.name for f in rv]
-        mock_request.assert_called_once()
+        paths = [f.path for f in rv]
+        # test URL *
+        mock_request.assert_called_once_with('get', '/storage/files/?path={path}'.format(
+                path=quote('*')))
+        # test received file names
         self.assertListEqual(names, ['foo', 'bar'])
+        # test received file paths
+        self.assertListEqual(paths, ['/foo', '/bar'])
+
+
+    @patch("dymaxionlabs.files.request")
+    def test_get(self, mock_request):
+        mock_request.return_value = {'detail':
+            {'name':'foo', 'path':'/foo', 'metadata':'metadata_foo'}
+        }
+        rv = File.get('/foo')
+        mock_request.assert_called_once_with('get', '/storage/file/?path={path}'.format(
+            path=quote('/foo')))
+        self.assertEqual(rv.name,'foo')
+        self.assertEqual(rv.path,'/foo') 
+        self.assertEqual(rv.metadata,'metadata_foo')
+    
+    @patch("dymaxionlabs.files.request")
+    def test_delete(self, mock_request):
+        mock_request.return_value = True
+        rv = File('foo', '/foo', 'metadata_foo').delete()
+        mock_request.assert_called_once_with('delete', '/storage/file/?path={path}'.format(
+            path=quote('/foo')))
+        self.assertTrue(rv)
+
+
+
