b3725a116da8ce488ee3c4eda7e7010067e131dc
Gessica <gessica@dymaxionlabs.com>
2020-07-13 17:34:16 -0300

    add test add_image and add_labels_for

--- tests/test_estimator.py
+++ tests/test_estimator.py
@@ -18,7 +18,6 @@ class EstimatorTest(unittest.TestCase):
                  'image_files':['f1','f2'], 'configuration':'conf1'}
         ], 'next':None}
         rv = Estimator.all()
-        print(rv)
         mock_request.assert_called_once_with('get', '/estimators/', params={})
         self.assertEqual(rv[0].uuid, 'u1')
         self.assertEqual(rv[0].name, 'n1')
@@ -77,3 +76,52 @@ class EstimatorTest(unittest.TestCase):
         ).delete()
         mock_request.assert_called_once_with('delete', '/estimators/u1')
         self.assertTrue(rv)
+
+    @patch("dymaxionlabs.models.request")
+    def test_add_image(self, mock_request):
+        class TestFile:
+            path = ''
+
+            def __init__(self, path):
+                self.path = path
+
+        est = Estimator(
+            uuid='u1',
+            name='n1',
+            classes='c1',
+            estimator_type='object_detection',
+            metadata='m1',
+            image_files=[TestFile('f1'), TestFile('f2')],
+            configuration='conf1'
+        )
+        rv = est.add_image(TestFile('f3'))
+        body = dict(image_files=[img.path for img in est.image_files])
+        mock_request.assert_called_once_with('patch', '/estimators/u1/', body)
+        self.assertIs(type(est), Estimator)
+
+    @patch("dymaxionlabs.models.request")
+    def test_add_labels_for(self, mock_request):
+        class TestFile:
+            path = ''
+
+            def __init__(self, path):
+                self.path = path
+
+        est = Estimator(
+            uuid='u1',
+            name='n1',
+            classes='c1',
+            estimator_type='object_detection',
+            metadata='m1',
+            image_files=[TestFile('f1'), TestFile('f2')],
+            configuration='conf1'
+        )
+        vector_file = TestFile('t1')
+        image_file = TestFile('i1')
+        label = 'c1'
+        est.add_labels_for(vector_file, image_file, label)
+        body = dict(vector_file=vector_file.path,
+            related_file=image_file.path,
+            label=label)
+        mock_request.assert_called_once_with('post', '/estimators/u1/load_labels', body)
+        self.assertIs(type(est), Estimator)
