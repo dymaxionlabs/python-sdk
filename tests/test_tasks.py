dc56a4176d18ad4f34da6355781e3194d179ca71
Gessica <gessica@dymaxionlabs.com>
2020-07-16 15:44:51 -0300

    fix task test

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -11,9 +11,8 @@ __license__ = "apache-2.0"
 
 class TasksTest(unittest.TestCase):
 
-    @patch("dymaxionlabs.tasks.request")
-    def test_is_running(self, mock_request):
-        task = Task(
+    def setUp(self):
+        self.task = Task(
             id="t1", 
             name="t1", 
             updated_at=None, 
@@ -24,38 +23,27 @@ class TasksTest(unittest.TestCase):
             args=None, 
             kwargs=None
         )
-        rv = task.is_running()
+
+    @patch("dymaxionlabs.tasks.Task.refresh")
+    def test_is_not_running(self, mock_refresh):
+        self.task.state = "FINISHED"
+        rv = self.task.is_running()
         self.assertEqual(rv, False)
-        mock_request.assert_not_called()
-        task.state = "RUNNING"
-        mock_request.return_value = dict(
-            id="t1", 
-            name="t1", 
-            updated_at=None, 
-            created_at=None, 
-            finished_at=None, 
-            state="RUNNING",
-            metadata=None, 
-            args=None, 
-            kwargs=None
-        )
-        rv = task.is_running()
-        mock_request.assert_called_once_with('get', '/tasks/t1')
+        self.task.state = "FAILED"
+        rv = self.task.is_running()
+        self.assertEqual(rv, False)
+        mock_refresh.assert_not_called()
+
+    @patch("dymaxionlabs.tasks.Task.refresh")
+    def test_is_running(self, mock_refresh):
+        self.task.state = "RUNNING"
+        rv = self.task.is_running()
+        mock_refresh.assert_called_once()
         self.assertEqual(rv, True)
 
     @patch("dymaxionlabs.tasks.request")
     def test_refresh(self, mock_request):
-        task = Task(
-            id="t1", 
-            name="t1", 
-            updated_at=None, 
-            created_at=None, 
-            finished_at=None, 
-            state="FINISHED",
-            metadata=None, 
-            args=None, 
-            kwargs=None
-        )
+        self.task.state = "FINISHED"
         mock_request.return_value = dict(
             id="t1", 
             name="t1", 
@@ -67,6 +55,6 @@ class TasksTest(unittest.TestCase):
             args=None, 
             kwargs=None
         )
-        rv = task.refresh()
+        rv = self.task.refresh()
         mock_request.assert_called_once_with('get', '/tasks/t1')
         self.assertEqual(rv.state, "RUNNING")
