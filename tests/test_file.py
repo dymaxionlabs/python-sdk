5af931e72b0d86e07cf5b7ef2d9721b637930985
Damián Silvani <munshkr@gmail.com>
2020-07-26 20:24:15 -0300

    Format test files

--- tests/test_file.py
+++ tests/test_file.py
@@ -1,51 +1,61 @@
 import unittest
-from requests.utils import quote
 from unittest.mock import patch
+
+from requests.utils import quote
+
 from dymaxionlabs.files import File
 
 __author__ = "Dymaxion Labs"
 __copyright__ = "Dymaxion Labs"
 __license__ = "apache-2.0"
 
-class FileTest(unittest.TestCase):
 
+class FileTest(unittest.TestCase):
     @patch("dymaxionlabs.files.request")
     def test_all(self, mock_request):
         mock_request.return_value = [
-            {'name':'foo', 'path':'/foo', 'metadata':'metadata_foo'},
-            {'name':'bar', 'path':'/bar', 'metadata':'metadata_bar'},
+            {
+                'name': 'foo',
+                'path': '/foo',
+                'metadata': 'metadata_foo'
+            },
+            {
+                'name': 'bar',
+                'path': '/bar',
+                'metadata': 'metadata_bar'
+            },
         ]
         rv = File.all()
         names = [f.name for f in rv]
         paths = [f.path for f in rv]
         # test URL *
-        mock_request.assert_called_once_with('get', '/storage/files/?path={path}'.format(
-                path=quote('*')))
+        mock_request.assert_called_once_with(
+            'get', '/storage/files/?path={path}'.format(path=quote('*')))
         # test received file names
         self.assertListEqual(names, ['foo', 'bar'])
         # test received file paths
         self.assertListEqual(paths, ['/foo', '/bar'])
 
-
     @patch("dymaxionlabs.files.request")
     def test_get(self, mock_request):
-        mock_request.return_value = {'detail':
-            {'name':'foo', 'path':'/foo', 'metadata':'metadata_foo'}
+        mock_request.return_value = {
+            'detail': {
+                'name': 'foo',
+                'path': '/foo',
+                'metadata': 'metadata_foo'
+            }
         }
         rv = File.get('/foo')
-        mock_request.assert_called_once_with('get', '/storage/file/?path={path}'.format(
-            path=quote('/foo')))
-        self.assertEqual(rv.name,'foo')
-        self.assertEqual(rv.path,'/foo') 
-        self.assertEqual(rv.metadata,'metadata_foo')
-    
+        mock_request.assert_called_once_with(
+            'get', '/storage/file/?path={path}'.format(path=quote('/foo')))
+        self.assertEqual(rv.name, 'foo')
+        self.assertEqual(rv.path, '/foo')
+        self.assertEqual(rv.metadata, 'metadata_foo')
+
     @patch("dymaxionlabs.files.request")
     def test_delete(self, mock_request):
         mock_request.return_value = True
         rv = File('foo', '/foo', 'metadata_foo').delete()
-        mock_request.assert_called_once_with('delete', '/storage/file/?path={path}'.format(
-            path=quote('/foo')))
+        mock_request.assert_called_once_with(
+            'delete', '/storage/file/?path={path}'.format(path=quote('/foo')))
         self.assertTrue(rv)
-
-
-
--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -1,28 +1,27 @@
 import unittest
-from requests.utils import quote
 from unittest.mock import patch
-from dymaxionlabs.tasks import Task
+
+from requests.utils import quote
 
 import dymaxionlabs
+from dymaxionlabs.tasks import Task
 
 __author__ = "Dymaxion Labs"
 __copyright__ = "Dymaxion Labs"
 __license__ = "apache-2.0"
 
-class TasksTest(unittest.TestCase):
 
+class TasksTest(unittest.TestCase):
     def setUp(self):
-        self.task = Task(
-            id="t1", 
-            name="t1", 
-            updated_at=None, 
-            created_at=None, 
-            finished_at=None, 
-            state="FINISHED",
-            metadata=None, 
-            args=None, 
-            kwargs=None
-        )
+        self.task = Task(id="t1",
+                         name="t1",
+                         updated_at=None,
+                         created_at=None,
+                         finished_at=None,
+                         state="FINISHED",
+                         metadata=None,
+                         args=None,
+                         kwargs=None)
 
     @patch("dymaxionlabs.tasks.Task.refresh")
     def test_is_not_running(self, mock_refresh):
@@ -44,17 +43,15 @@ class TasksTest(unittest.TestCase):
     @patch("dymaxionlabs.tasks.request")
     def test_refresh(self, mock_request):
         self.task.state = "FINISHED"
-        mock_request.return_value = dict(
-            id="t1", 
-            name="t1", 
-            updated_at=None, 
-            created_at=None, 
-            finished_at=None, 
-            state="RUNNING",
-            metadata=None, 
-            args=None, 
-            kwargs=None
-        )
+        mock_request.return_value = dict(id="t1",
+                                         name="t1",
+                                         updated_at=None,
+                                         created_at=None,
+                                         finished_at=None,
+                                         state="RUNNING",
+                                         metadata=None,
+                                         args=None,
+                                         kwargs=None)
         rv = self.task.refresh()
         mock_request.assert_called_once_with('get', '/tasks/t1')
         self.assertEqual(rv.state, "RUNNING")
